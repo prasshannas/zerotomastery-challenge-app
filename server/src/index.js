68f33ad60eddb1bbb14b67c9a31bf89c60ce84ae
Theodoros Gkortsilas <theodoros.gkortsilas.2014@gmail.com>
2020-02-06 21:32:30 +0000

    connect DB

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,16 @@
+require('dotenv').config();
+
 const app = require('./app');
 const config = require('../config');
+
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch((err) => console.log('Unable to connect to the database:', error));
 
 app.server.listen(config.port);
