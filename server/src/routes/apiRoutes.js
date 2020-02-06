9f35b8c6b79f290e944333b457212131d95cb830
Rashad Ibrahimov <i.redjee@gmail.com>
2020-02-06 22:56:58 +0400

    Improve prod scripts

--- server/src/routes/apiRoutes.js
+++ server/src/routes/apiRoutes.js
@@ -1,5 +1,5 @@
 module.exports = (apiRouter) => {
   /* eslint-disable global-require */
-  require('./components/welcome/welcome.router')(apiRouter);
+  require('../components/welcome/welcome.router')(apiRouter);
   /* eslint-enable global-require */
 };
