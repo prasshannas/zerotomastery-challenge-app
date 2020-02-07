b330f147b9f87ae77cc5ebd3606a14dc51a4bbf2
Theodoros Gkortsilas <theodoros.gkortsilas.2014@gmail.com>
2020-02-07 16:55:07 +0000

    restructure db folders and files, delete demo db users

--- server/config/test.js
+++ server/config/test.js
@@ -1,3 +1,9 @@
 module.exports = {
   port: 3000,
+  username: 'root',
+  password: null,
+  database: 'boilerplate_test',
+  host: '127.0.0.1',
+  dialect: 'postgres',
+  logging: null,
 };
--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,10 @@
 require('dotenv').config();
 
 const app = require('./app');
+const db = require('../database');
 
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres'
-});
-
-sequelize.authenticate()
+db.authenticate()
   .then(() => console.log('Connection has been established successfully.'))
-  .catch((err) => console.log('Unable to connect to the database:', error));
+  .catch((err) => console.log('Unable to connect to the database:', err));
 
 app.server.listen(app.config.port);
