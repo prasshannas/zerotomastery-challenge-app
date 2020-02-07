1e5913847d21e744b4b5cf1fff0b20f7ac96d5a1
Theodoros Gkortsilas <theodoros.gkortsilas.2014@gmail.com>
2020-02-07 17:51:49 +0000

    some more file changes and cleanups

--- server/database/models/index.js
+++ server/database/models/index.js
@@ -6,12 +6,10 @@ const config = require('../../config');
 const basename = path.basename(__filename);
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: config.dialect,
+});
 
 fs
   .readdirSync(__dirname)
--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 const app = require('./app');
-const db = require('../database');
+const db = require('../database/models');
 
-db.authenticate()
+db.sequelize.authenticate()
   .then(() => console.log('Connection has been established successfully.'))
   .catch((err) => console.log('Unable to connect to the database:', err));
 
