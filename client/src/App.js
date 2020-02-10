548db17501149ed47c1c5d8737c8ff5c5c05df8d
mansour.malloum <mansourmalloum@gmail.com>
2020-02-10 18:40:33 +0100

    We have fixed conflicts issues

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import ChallengePage from "./pages/challenges/challengepage.components";
 import Challenge from "./components/challenge/challenge.component";
 import "./App.scss";
 
-<<<<<<< HEAD
 const App = () => (
     <div className='app'>
         <Header />
@@ -20,22 +19,4 @@ const App = () => (
     </div>
 );
 
-=======
-
-const App = () => {
-    return ( 
-      <div className = 'app'>
-        <Header />
-        <Switch >
-           <Route exact path = '/' component = { HomePage }  /> 
-            <Route path = '/login' component = { LoginPage }/> 
-            <Route path = '/challenges' component = { ChallengePage }/>
-         </Switch >
-         </div>
-    );
-};
-
-
-
->>>>>>> e35a106c62d0fea126667aa017fba16294fba07b
 export default App;
